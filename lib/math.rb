329f8795d7f192f69962d047f4a78b7bfc814573
Joseph S. Cha <josephshincha@gmail.com>
2020-01-20 20:32:34 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,23 +3,21 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  return num1.to_i - num2.to_i
 end
 
 def division(num1, num2)
-
+  return num1.to_i / num2.to_i
 end
 
 def multiplication(num1, num2)
-
+  return num1.to_i * num2.to_i
 end
 
 def modulo(num1, num2)
-
+  return num1.to_i % num2.to_i
 end
 
 def square_root(num)
-
+  return num1.to_i ** num2.to_i
 end
-
-addition(4,5)
